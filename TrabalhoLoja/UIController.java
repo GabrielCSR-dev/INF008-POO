aad4e37557ab99bfc978dc3d9fd4572775ada041
Gabriel Cabral de Santana Ribeiro <166030912+GabrielCSR-dev@users.noreply.github.com>
2024-12-05 19:39:44 -0300

    Update UIController.java

--- TrabalhoLoja/UIController.java
+++ TrabalhoLoja/UIController.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 class UIController{
+    private Scanner scanner = new Scanner(System.in);
+    public static String[] loginMenu(){
+        String[2] login = new String[2]();
+        System.out.println("[Login Menu]\nInsert your e-mail:");
+        login[0] = scanner.nextLine();
+        System.out.println("Insert your password:");
+        login[1] = scanner.nextLine();
+        return login;
+    }
     public static void customerMenu(){
         System.out.println("[Customer Menu]");
         System.out.println("Please, select an option:");
