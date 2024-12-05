fa7e01193c72f7bbebc668aa066bcfe9a7b390da
Gabriel Cabral de Santana Ribeiro <166030912+GabrielCSR-dev@users.noreply.github.com>
2024-12-05 19:18:52 -0300

    Update Administrator.java

--- TrabalhoLoja/Administrator.java
+++ TrabalhoLoja/Administrator.java
@@ -2,7 +2,7 @@ class Administrator extends User{
     public Administrator(String name, String email, String password) throws Exception{
         super(name, email, password);
     }
-    public void test(){
+    public void chooseOption(){
         System.out.println("I'm an administrator!");
     }
 }
